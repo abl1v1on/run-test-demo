fbaa608331e16ba2da0c91c185d3fbca763970e6
Maxim Danilov <abl1v1on@yandex.ru>
2024-10-02 10:04:07 +0500

    add chrome options

--- conftest.py
+++ conftest.py
@@ -1,11 +1,17 @@
 import pytest
 from typing import Generator
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+
+def get_chrome_options() -> Options:
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    return chrome_options
 
 @pytest.fixture
 def browser() -> Generator:
-    chrome_browser = webdriver.Chrome()
+    chrome_browser = webdriver.Chrome(options=get_chrome_options())
     chrome_browser.maximize_window()
     yield chrome_browser
     chrome_browser.quit()
